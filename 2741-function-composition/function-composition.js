593d801e61fe11a372ecf9d0f8ec8f2b38c123e0
Santhosh Yerru <santhoshnaidu345@gmail.com>
2024-07-14 00:07:51 -0400

    Time: 60 ms (77.58%) | Memory: 49.7 MB (88.83%) - LeetSync

---
+++ 2741-function-composition/function-composition.js
@@ -0,0 +1,20 @@
+/**
+ * @param {Function[]} functions
+ * @return {Function}
+ */
+var compose = function(functions) {
+    
+    return function(x) {
+        if(functions.length===0){
+            return x;
+        }
+        return functions.reduceRight((acc, curr)=>{
+            return curr(acc);
+        }, x);
+    }
+};
+
+/**
+ * const fn = compose([x => x + 1, x => 2 * x])
+ * fn(4) // 9
+ */

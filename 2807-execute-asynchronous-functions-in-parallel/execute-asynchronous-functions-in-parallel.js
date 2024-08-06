c5ca3be2d5f69a3f5beb87f09af96ea703cd1215
Santhosh Yerru <santhoshnaidu345@gmail.com>
2024-08-06 00:19:47 -0400

    Time: 67 ms (79.92%) | Memory: 50.2 MB (5.44%) - LeetSync

---
+++ 2807-execute-asynchronous-functions-in-parallel/execute-asynchronous-functions-in-parallel.js
@@ -0,0 +1,27 @@
+/**
+ * @param {Array<Function>} functions
+ * @return {Promise<any>}
+ */
+var promiseAll = function(functions) {
+    return new Promise((resolve, reject)=>{
+        let results = new Array(functions.length);
+        let count = 0;
+        functions.forEach((fn, i)=>{
+            fn().then((val)=>{
+                results[i] = val;
+                count++;
+                if(count===functions.length) 
+                    return resolve(results);
+            } 
+            ).catch(
+                (reason)=>reject(reason)
+            )
+        })
+    })
+    
+};
+
+/**
+ * const promise = promiseAll([() => new Promise(res => res(42))])
+ * promise.then(console.log); // [42]
+ */

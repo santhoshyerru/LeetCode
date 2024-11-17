f96640c16fa13a58caf7d910f7fbfdf042972375
Santhosh Yerru <santhoshnaidu345@gmail.com>
2024-11-16 19:02:31 -0500

    Time: 1 ms (100.00%) | Memory: 45.7 MB (13.30%) - LeetSync

--- 3522-find-the-power-of-k-size-subarrays-i/find-the-power-of-k-size-subarrays-i.java
+++ 3522-find-the-power-of-k-size-subarrays-i/find-the-power-of-k-size-subarrays-i.java
@@ -1,29 +1,27 @@
 class Solution {
     public int[] resultsArray(int[] nums, int k) {
-        int n = nums.length;
-        int[] ans = new int[n - k + 1];  // Initialize result array with size (n - k + 1)
-        for (int i = 0; i < ans.length; i++) {
-            ans[i] = -1;  // Fill the array with -1 as the default value
+        int[] res = new int[nums.length-k+1];
+        int i = 0,  j = 0, p=0, count=1;
+        while(j<nums.length){
+            if(j>0 && nums[j]==nums[j-1]+1){
+                count++;
+            }
+            if(j-i+1>k){
+                if(nums[i+1]==nums[i]+1){
+                    count--;
+                }
+                i++;
+            }
+            if(j-i+1==k){
+                if(count==k){
+                    res[p++] = nums[j];
+                }else{
+                    res[p++] = -1;
+                }
+            }
+
+            j++;
         }
-
-        int i = 0, j = 0;
-        while (j < n) {
-            if (j > 0 && nums[j] - nums[j - 1] != 1) {
-                i = j;  // Reset the 'i' index if the difference is not 1
-            }
-
-            // Ensure the window size is not greater than k
-            while (i < j && j - i + 1 > k) {
-                i++;  // Shrink the window from the left
-            }
-
-            // If we have exactly k elements in the window, update the answer array
-            if (j - i + 1 == k) {
-                ans[j - k + 1] = nums[j];  // Store the current element in the result array
-            }
-            j++;  // Move the right pointer
-        }
-
-        return ans;
+        return res;
     }
 }
